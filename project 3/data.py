d0c79789ef9ed83dfdd0ddbfcaaac4e9b659375c
Matthew Silverman <Matthew Silverman>
2017-01-26 17:43:15 -0500

    almost there

--- project 3/data.py
+++ project 3/data.py
@@ -4,13 +4,18 @@ from sklearn.utils import shuffle
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import cv2, numpy as np
 
 def getData(data_path):
-    csv_path = data_path + "driving_log.csv"
-    img_path = data_path + "IMG/"
+    # csv_path = data_path + "driving_log.csv"
+    csv_path = data_path + "gasoto_log.csv"
+    # img_path = data_path + "IMG/"
+    steer_offset = .25
 
     with open(csv_path, 'r') as f:
         reader = csv.reader(f)
+        X_train = []
+        y_train = []
         csvRows = []
         leftImagePaths = []
         rightImagePaths = []
@@ -18,38 +23,49 @@ def getData(data_path):
         steeringValue = []
         row = next(reader) #move past the header row
         for row in reader: #format: center name, left name, right name, steering, throttle, brake, speed
-            row[0] = data_path + row[0]
-            row[1] = data_path + row[1]
-            row[2] = data_path + row[2]
-            csvRows.append(row)
+            # row[0] = data_path + row[0]
+            # row[1] = data_path + row[1]
+            # row[2] = data_path + row[2]
+            X_train.append(row[0])
+            y_train.append(float(row[3]))
+            X_train.append(row[1])
+            y_train.append(float(row[3]) - steer_offset)
+            X_train.append(row[2])
+            y_train.append(float(row[3]) + steer_offset)
+            # csvRows.append(row)
 
     #Split into training and testing sets
-    csvRows = shuffle(csvRows)
-    csvTrain, csvTest = train_test_split(csvRows, test_size = 0.2)
-    X_train = [item[0] for item in csvTrain]
-    y_train = [item[3] for item in csvTrain]
-    X_test = [item[0] for item in csvTest]
-    y_test = [item[3] for item in csvTest]
+    # # csvRows = shuffle(csvRows)
+    # csvTrain, csvTest = train_test_split(csvRows, test_size = 0.2)
+    # X_train = [item[0] for item in csvTrain]
+    # y_train = [item[3] for item in csvTrain]
+    # X_test = [item[0] for item in csvTest]
+    # y_test = [item[3] for item in csvTest]
+
+    X_train, y_train = shuffle(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train)
     return(X_train, y_train, X_test, y_test)
 
 
 def generator_data(data, angle, batch_size):
     index = np.arange(len(data))
     while 1:
-        batch_train = np.zeros((batch_size, 160, 320, 3), dtype = np.float32)
-        batch_angle = np.zeros((batch_size), dtype = np.float32)
+        # batch_train = np.zeros((batch_size, 160, 320, 3), dtype = np.float32)
+        batch_train = np.zeros((batch_size, 40, 160, 3), dtype = np.float32)
+        batch_angle = np.zeros((batch_size,), dtype = np.float32)
         for i in range(batch_size):
             try:
-                random = int(np.random.choice(data.shape[0],1))
+                random = int(np.random.choice(index,1))
             except:
                 index = np.arange(len(data))
                 batch_train = batch_train[:i,:,:]
                 batch_angle = batch_angle[:i]
                 break
             batch_train[i] = cv2.imread(data[random])
-            batch_train[i] = batch_train[i] - np.mean(batch_train[i])
+            # batch_train[i] = batch_train[i] - np.mean(batch_train[i])
             batch_angle[i] = angle[random]
         yield (batch_train, batch_angle)
+
 
 def load_image(image_path):
     img = mpimg.imread(image_path)
--- project 3/model.py
+++ project 3/model.py
@@ -5,16 +5,23 @@ from keras.optimizers import SGD
 from keras.layers.normalization import BatchNormalization
 import cv2, numpy as np
 from data import *
+from keras.models import model_from_json
 
-def nvidia(inputShape):
+def nvidia_model(inputShape):
     model = Sequential()
     model.add(BatchNormalization(input_shape=inputShape))
 
-    model.add(Convolution2D(24, 5, 5, border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Convolution2D(36, 5, 5, border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Convolution2D(48, 3, 3, border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu', subsample=(2,2)))
+    model.add(Convolution2D(24, 5, 5, border_mode='same', activation='relu', subsample=(2,2)))
+    model.add(Convolution2D(36, 5, 5, border_mode='same', activation='relu', subsample=(2,2)))
+    model.add(Convolution2D(48, 3, 3, border_mode='same', activation='relu', subsample=(2,2)))
+    model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu', subsample=(2,2)))
+    model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu', subsample=(2,2)))
+
+    # model.add(Convolution2D(24, 5, 5, activation='relu', subsample=(2,2)))
+    # model.add(Convolution2D(36, 5, 5, activation='relu', subsample=(2,2)))
+    # model.add(Convolution2D(48, 3, 3, activation='relu', subsample=(2,2)))
+    # model.add(Convolution2D(64, 3, 3, activation='relu', ))
+    # model.add(Convolution2D(64, 3, 3, activation='relu', ))
 
     model.add(Flatten())
     model.add(Dense(1164, activation='relu'))
@@ -31,9 +38,13 @@ print("Size of training set:", len(X_train))
 print("Size of testing set:", len(X_test))
 input("Press ENTER to continue")
 
-model = nvidia((160, 320, 3))
+# model = nvidia_model((160, 320, 3))
+model = nvidia_model((40, 160, 3))
 model.compile('adam', 'mse')
 print("Model compiled!")
 
 input("Press ENTER to train")
 history = model.fit_generator(generator_data(X_train, y_train,128), nb_epoch=1, verbose=1, samples_per_epoch=len(X_train), validation_data=(generator_data(X_test, y_test, 128)),nb_val_samples=len(X_test))
+with open('model.json', 'w') as f:
+    f.write(model.to_json())
+model.save_weights('model.h5')
